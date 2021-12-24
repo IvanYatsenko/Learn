fea450803875ffefb1c7e71b65ff4b1548c14531
Ivan Yatsenko <yatsenko.ivan@rambler.ru>
2021-12-24 23:35:14 +0700

    main

--- Andrievskyi/JS/Lesson4/js/my.js
+++ Andrievskyi/JS/Lesson4/js/my.js
@@ -1,12 +1,22 @@
-let x = '3';
-switch(x) {
-    case 1: alert('1');
-        break;
-    case 2: alert('2');
+var x = prompt('Введите первое число', 100);
+var y = prompt('Введите второе число', 100);
+var z = prompt('Введите 1 для "+", 2 для "-", 3 чтобы умножить и 4 для деления.');
+
+x = parseInt(x);
+y = parseInt(y);
+
+var result;
+
+switch(z) {
+    case '1': result = x + y;
     break;
-    case 3: alert('3');
-        break;
-    case 4: alert('4');
-        break;
-    default: alert('No much');
+    case '2': result = x - y;
+    break;
+    case '3': result = x * y;
+    break;
+    case '4': result = x / y;
+    break;
+    default: result = 'Неверные данные!';
 }
+
+alert(result);
---
+++ Andrievskyi/JS/Lesson5/js/my.js
@@ -0,0 +1,32 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+
+var hits = 0;
+
+var guesses = 0;
+
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt('Fire at 0 to 6:');
+    if (guess < 0 || guess > 6) {
+        alert('Number at 0 to 6!')
+    } else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert('You win!')
+            }
+        } else {
+            alert('you dont hits')
+        }
+    }
+}
+
+var status = `You hits ${guesses} to ship!`;
+alert(status);
