ebcaa263292c8cfc14664e4a78e8d273a1f31a08
Ivan Yatsenko <79698502+IvanYatsenko@users.noreply.github.com>
2021-12-26 02:18:25 +0700

    main

--- Andrievskyi/JS/Lesson7/js/my.js
+++ Andrievskyi/JS/Lesson7/js/my.js
@@ -1,30 +1,44 @@
 
-var location1 = Math.floor(Math.random() * 5);
-var location2 = location1 + 1;
-var location3 = location2 + 1;
+// var location1 = Math.floor(Math.random() * 5);
+// var location2 = location1 + 1;
+// var location3 = location2 + 1;
 
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
+// var guess;
+// var hits = 0;
+// var guesses = 0;
+// var isSunk = false;
 
-while (isSunk == false) {
-    guess = prompt('Fire at 0 to 6:');
-    if (guess < 0 || guess > 6) {
-        alert('Number at 0 to 6!')
+// while (isSunk == false) {
+//     guess = prompt('Fire at 0 to 6:');
+//     if (guess < 0 || guess > 6) {
+//         alert('Number at 0 to 6!')
+//     } else {
+//         guesses = guesses + 1;
+//         if (guess == location1 || guess == location2 || guess == location3) {
+//             hits = hits + 1;
+//             if (hits == 3) {
+//                 isSunk = true;
+//                 alert('You win!')
+//             }
+//         } else {
+//             alert('you dont hits')
+//         }
+//     }
+// }
+
+// var status = `You hits ${guesses} to ship!`;
+// alert(status);
+
+
+function checkStudent(name, age) {
+    if (age > 17) {
+        console.log(name + " уже окончил школу!")
     } else {
-        guesses = guesses + 1;
-        if (guess == location1 || guess == location2 || guess == location3) {
-            hits = hits + 1;
-            if (hits == 3) {
-                isSunk = true;
-                alert('You win!')
-            }
-        } else {
-            alert('you dont hits')
-        }
+        console.log(name + " ещё учится в школе.")
     }
 }
 
-var status = `You hits ${guesses} to ship!`;
-alert(status);
+checkStudent('Nick', 23);
+checkStudent('Make', 15);
+checkStudent('Pit', 17);
+checkStudent('Kate', 53);
