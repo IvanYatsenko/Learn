d7dbfdc967e69ff95b686dadb90a6469d8c1b440
Ivan Yatsenko <79698502+IvanYatsenko@users.noreply.github.com>
2022-01-03 03:14:38 +0700

    main

---
+++ Andrievskyi/JS/Lesson10/js/my.js
@@ -0,0 +1,30 @@
+var chevi = {
+    make: 'Chevi',
+    model: 'Aveo',
+    year: 2010,
+    color: 'red',
+    passengers: 4,
+    convertible: false,
+    mileage: 1021
+};
+
+var cadi = {
+    make: 'Cadillac',
+    model: 'GM',
+    year: 1955,
+    color: 'yellow',
+    passengers: 2,
+    convertible: true,
+    mileage: 112021   
+};
+
+console.log(cadi);
+console.log(chevi.color);
+
+if(chevi.year >= 2010) {
+    var money = 10000;
+} else {
+    var money = 13000;
+}
+
+console.log(money);
